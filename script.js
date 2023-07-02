7d273d5f2f54b8c51e2b91666db92c409cb44233
Ezequiel Bernard <ezequielbernard2@gmail.com>
2023-07-02 18:28:18 -0300

    v0

---
+++ script.js
@@ -0,0 +1,91 @@
+const CLAVE_LOCALSTORAGE = "listado_tareas";
+
+document.addEventListener("DOMContentLoaded", () => {
+  let tareas = [];
+  const $contenedorTareas = document.querySelector("#contenedorTareas"),
+    $btnGuardarTarea = document.querySelector("#AgregarTarea"),
+    $NuevaTarea = document.querySelector("#NuevaTarea"),
+    $contadorTareas = document.querySelector("#contadorTareas");
+
+  $btnGuardarTarea.onclick = () => {
+    const tarea = $NuevaTarea.value;
+    if (!tarea) {
+      return;
+    }
+    tareas.push({
+      tarea: tarea,
+      terminada: false,
+    });
+    $NuevaTarea.value = "";
+    guardarTareas();
+    refrescarTareas();
+  };
+
+  const obtenerTareas = () => {
+    const posibleLista = JSON.parse(localStorage.getItem(CLAVE_LOCALSTORAGE));
+    if (posibleLista) {
+      return posibleLista;
+    } else {
+      return [];
+    }
+  };
+
+  const guardarTareas = () => {
+    localStorage.setItem(CLAVE_LOCALSTORAGE, JSON.stringify(tareas));
+  };
+
+  const refrescarTareas = () => {
+    $contenedorTareas.innerHTML = "";
+    let contadorPendientes = 0; 
+
+    for (const [indice, tarea] of tareas.entries()) {
+      const $enlaceParaEliminar = document.createElement("a");
+      $enlaceParaEliminar.classList.add("enlace-eliminar");
+      $enlaceParaEliminar.innerHTML = "&times;";
+      $enlaceParaEliminar.href = "";
+      $enlaceParaEliminar.onclick = (evento) => {
+        evento.preventDefault();
+        if (!confirm("¿Estás seguro/a que deseas eliminar la tarea?")) {
+          return;
+        }
+        tareas.splice(indice, 1);
+        guardarTareas(tareas);
+        refrescarTareas();
+      };
+
+      const $checkbox = document.createElement("input");
+      $checkbox.type = "checkbox";
+      $checkbox.onchange = function () {
+        if (this.checked) {
+          tareas[indice].terminada = true;
+        } else {
+          tareas[indice].terminada = false;
+        }
+        guardarTareas(tareas);
+        refrescarTareas();
+      };
+
+      const $span = document.createElement("span");
+      $span.textContent = tarea.tarea;
+
+      const $li = document.createElement("li");
+      if (tarea.terminada) {
+        $checkbox.checked = true;
+        $span.classList.add("tachado");
+      }
+      $li.appendChild($checkbox);
+      $li.appendChild($span);
+      $li.appendChild($enlaceParaEliminar);
+      $contenedorTareas.appendChild($li);
+
+      if (!tarea.terminada) {
+        contadorPendientes++;
+      }
+    }
+
+    $contadorTareas.textContent = `Tareas Pendientes: ${contadorPendientes}`;
+  };
+
+  tareas = obtenerTareas();
+  refrescarTareas();
+});
